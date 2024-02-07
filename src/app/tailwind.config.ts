70c0b58eda8815842b31b3d8537d993eac941032
John Doe <johndoe@example.com>
2024-02-07 10:24:06 +0100

    ajout components

---
+++ src/app/tailwind.config.ts
@@ -0,0 +1,8 @@
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'], // Chemins vers tous vos fichiers utilisant Tailwind CSS
+    theme: {
+      extend: {}, // Personnalisez votre thème ici
+    },
+    plugins: [], // Ajoutez des plugins Tailwind ici si nécessaire
+  };
+  
